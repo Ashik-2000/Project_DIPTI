2cf42e516a8df2318253e77b281122c1c4bbc9c0
Ashikul Islam <ashikulislamfb@gmail.com>
2025-05-07 01:17:20 +0600

    Network Error done

--- ecommerce_frontend/src/components/home/HomePage.jsx
+++ ecommerce_frontend/src/components/home/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import api from "../../api";
+import Error from "../ui/Error";
 import PlaceHolderContainer from "../ui/PlaceHolderContainer";
 import CardContainer from "./CardContainer";
 import Header from "./Header";
@@ -7,6 +8,7 @@ import Header from "./Header";
 const HomePage = () => {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
 
     useEffect(function () {
         setLoading(true);
@@ -15,21 +17,21 @@ const HomePage = () => {
                 console.log(res.data);
                 setProducts(res.data);
                 setLoading(false);
+                setError("");
             })
             .catch((err) => {
                 console.log(err.message);
                 setLoading(false);
+                setError(err.message);
             });
     }, []);
 
     return (
         <>
             <Header />
-            {loading ? (
-                <PlaceHolderContainer />
-            ) : (
-                <CardContainer products={products} />
-            )}
+            {error && <Error error={error} />}
+            {loading && <PlaceHolderContainer />}
+            {loading || error != "" || <CardContainer products={products} />}
         </>
     );
 };
