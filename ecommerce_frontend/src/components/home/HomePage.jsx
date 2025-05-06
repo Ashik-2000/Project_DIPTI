787afdc54bfe23bcc7f94011854147d7908b1491
Ashikul Islam <ashikulislamfb@gmail.com>
2025-05-07 01:06:50 +0600

    loading effect done

--- ecommerce_frontend/src/components/home/HomePage.jsx
+++ ecommerce_frontend/src/components/home/HomePage.jsx
@@ -1,26 +1,35 @@
 import { useEffect, useState } from "react";
 import api from "../../api";
+import PlaceHolderContainer from "../ui/PlaceHolderContainer";
 import CardContainer from "./CardContainer";
 import Header from "./Header";
 
 const HomePage = () => {
     const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(function () {
+        setLoading(true);
         api.get("products")
             .then((res) => {
                 console.log(res.data);
                 setProducts(res.data);
+                setLoading(false);
             })
             .catch((err) => {
                 console.log(err.message);
+                setLoading(false);
             });
     }, []);
 
     return (
         <>
             <Header />
-            <CardContainer products={products} />
+            {loading ? (
+                <PlaceHolderContainer />
+            ) : (
+                <CardContainer products={products} />
+            )}
         </>
     );
 };
